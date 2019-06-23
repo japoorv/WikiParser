286fea5efe2ff9f781008ab793ec77912fb43068
japoorv <japoorv0@gmail.com>
2019-06-23 12:58:41 +0530

    Update parser.py
    1.) Added the functionality of extracting only the main articles from the wikipedia dump . Refer to the Readme.md point 1 for more information.

--- parser.py
+++ parser.py
@@ -7,6 +7,15 @@ revision=0;
 contributor=0;
 temp_pid='';
 progrss=0;
+tag=0;
+ns='';
+
+def conv(a):
+	b='';
+	for i in range(0,len(a)):
+		if (a[i].isdigit()):
+			b+=a[i];
+	return str(b);
 class MyHandler(xml.sax.handler.ContentHandler):
 	def __init__(self):
 		xml.sax.ContentHandler.__init__(self);
@@ -15,6 +24,7 @@ class MyHandler(xml.sax.handler.ContentHandler):
 	def startElement(self,name,attr):
 		global pid;
 		global revision;
+		global tag;
 		global contributor;
 		self.tagname=name;
 		if (name=='revision'):
@@ -25,37 +35,58 @@ class MyHandler(xml.sax.handler.ContentHandler):
 			pid='';
 		elif (name=='id' and revision==1 and contributor==0):
 			pid+='_'
+		if (name=='text' or tag!=0):
+			tag+=1;
+		
 		
 	def endElement(self,name):
 		global progrss;
 		global page_text;
 		global pid;
 		global revision;
+		global tag;
 		global contributor;
+		global ns;
 		global temp_pid;
-		if (name=='text'):
+		if (name=='text' and ns==0):
 			wparser.parse_text(page_text,pid); #Parses the given text
 			page_text='';
 			contributor=0;
 			revision=0;
 			progrss+=1;
-			print (progrss);
+
+		
 
 		elif(name=='id' and contributor==0):
 			pid+=str(int(temp_pid));
 			temp_pid='';
+
+		elif (name=='ns'):
+			ns=int(ns)
+		if (tag>0):
+			tag-=1;
 
 
 	def characters(self,content):
 		global page_text;
 		global pid;
 		global temp_pid;
+		global ns;
 		if (self.tagname=='id' and contributor==0):
 			temp_pid+=content;
-		elif (self.tagname=='text'):
+		elif (self.tagname=='text' or tag==1):
 			page_text+=content;
+		elif (self.tagname=='ns'):
+			bb=conv(content);
+			if (bb!=''):
+				ns=bb;
+				
+			
+	
+			
 
 def main():
+	global page_text
 	tic=time.time();
 	parser=xml.sax.make_parser()
 	handler=MyHandler()
